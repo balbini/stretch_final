a4855bad4519b76079ca23c22e1fd691f8bfe9a6
balbini <balbini91@gmail.com>
2018-05-29 22:39:20 -0700

    views updated for devise, application, and pages

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -22,12 +22,50 @@ class WorkoutController < ApplicationController
       @workout = Workout.find_by_id(params[:id])
   end
 
+  def edit
+    if user_check
+      @workout = Workout.find_by_id(params[:id])
+    else
+      flash[:notice] = "Cannot edit other workouts"
+    end
+  end
+
+  def update
+    if user_check
+      @workout = Workout.find_by_id(params[:id])
+      @workout.update(workout_params)
+      redirect_to workout_path
+    else
+      flash[:error] = "Cannot edit others workouts"
+      redirect_back(fallback_location: root_path)
+    end
+  end
+
+  def destroy
+    if user_check
+      @workout = Workout.find_by_id(params[:id])
+      @workout.destroy
+      redirect_to(workouts_path)
+    else
+      flash[:error] = "Cannot delete other workouts"
+      redirect_to root_path
+    end
+  end
+
+  def user_check
+    @workout = Workout.find_by_id(params[:id])
+    if current_user.id === @workout.user_id
+      true
+    else
+      false
+    end
+  end
 
   private
 
   def workout_params
   params.require(:workout).permit(:workout_type, :exercises, :experience, :user_id)
-end
+  end
 
 
 
